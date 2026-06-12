edd4efd3d799193619199ff51101a8bb035685bb
pratik-ashok <pratik.ashok@gmail.com>
2026-06-12 19:12:10 -0400

    Add PickupContact model for daycare management

---
+++ DaycareManagement/Models/PickupContact.cs
@@ -0,0 +1,9 @@
+namespace DaycareManagement.Models;
+
+public class PickupContact
+{
+    public int ChildId { get; set; }
+    public string Name { get; set; } = "";
+    public string Relationship { get; set; } = "";
+    public string Phone { get; set; } = "";
+}
