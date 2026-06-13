5882211e2dd7f3c1850bdd0f4e7c306b398ff470
pratik-ashok <pratik.ashok@gmail.com>
2026-06-12 22:26:04 -0400

    Add FR11/FR12: DailySummaryService and updated Program.cs for v3

--- DaycareManagement/Program.cs
+++ DaycareManagement/Program.cs
@@ -1,46 +1,44 @@
 using DaycareManagement.Menus;
 using DaycareManagement.Services;
 
-namespace DaycareManagement;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-class Program
+var store      = new DataStore(AppDomain.CurrentDomain.BaseDirectory);
+var attendance = new AttendanceService(store);
+var staffing   = new StaffingService(store);
+var allergy    = new AllergyService(store);
+var pickup     = new PickupService(store);
+var summary    = new DailySummaryService(store);
+
+var attendanceMenu = new AttendanceMenu(attendance, allergy, staffing);
+var staffingMenu   = new StaffingMenu(staffing);
+var allergyMenu    = new AllergyMenu(allergy, attendance);
+var pickupMenu     = new PickupMenu(pickup, attendance);
+var summaryMenu    = new DailySummaryMenu(summary, attendance, staffing, allergy);
+
+UI.PrintBanner();
+
+bool running = true;
+while (running)
 {
-    static void Main(string[] args)
+    UI.PrintHeader("Main Menu");
+    Console.WriteLine("  1. Children & Attendance   [FR2, FR3]");
+    Console.WriteLine("  2. Staffing Management     [FR4, FR5, FR6]");
+    Console.WriteLine("  3. Allergy Management      [FR7, FR8]");
+    Console.WriteLine("  4. Pickup Management       [FR9, FR10]");
+    Console.WriteLine("  5. Daily Summary           [FR11, FR12]");
+    Console.WriteLine("  0. Exit");
+    Console.Write("\n  Select: ");
+    switch (Console.ReadLine()?.Trim())
     {
-        Console.OutputEncoding = System.Text.Encoding.UTF8;
-        UI.PrintBanner();
-
-        var dataStore  = new DataStore(AppDomain.CurrentDomain.BaseDirectory);
-        var attendance = new AttendanceService(dataStore);
-        var allergy    = new AllergyService(dataStore);
-        var pickup     = new PickupService(dataStore);
-        var staffing   = new StaffingService(dataStore);
-
-        var attendanceMenu = new AttendanceMenu(attendance, allergy, staffing);
-        var allergyMenu    = new AllergyMenu(allergy, attendance);
-        var pickupMenu     = new PickupMenu(pickup, attendance);
-        var staffingMenu   = new StaffingMenu(staffing);
-
-        bool running = true;
-        while (running)
-        {
-            UI.PrintHeader("Main Menu");
-            Console.WriteLine("  1. Children & Attendance  [FR2, FR3]");
-            Console.WriteLine("  2. Allergy Management     [FR7, FR8]");
-            Console.WriteLine("  3. Pickup Management      [FR9, FR10]");
-            Console.WriteLine("  4. Staffing & Ratio       [FR4, FR5, FR6]");
-            Console.WriteLine("  0. Exit");
-            Console.Write("\n  Select: ");
-            switch (Console.ReadLine()?.Trim())
-            {
-                case "1": attendanceMenu.Run(); break;
-                case "2": allergyMenu.Run();    break;
-                case "3": pickupMenu.Run();     break;
-                case "4": staffingMenu.Run();   break;
-                case "0": running = false;      break;
-                default:  UI.PrintError("Invalid option."); break;
-            }
-        }
-        Console.WriteLine("\n  Goodbye!\n");
+        case "1": attendanceMenu.Run(); break;
+        case "2": staffingMenu.Run();   break;
+        case "3": allergyMenu.Run();    break;
+        case "4": pickupMenu.Run();     break;
+        case "5": summaryMenu.Run();    break;
+        case "0": running = false;      break;
+        default:  UI.PrintError("Invalid option."); break;
     }
 }
+
+Console.WriteLine("\n  Goodbye!\n");
